c648110cda9634c34e1b7d65cdbfe1448663658b
Zhida Zhang <zhangzhida822@outlook.com>
2019-01-29 23:01:18 -0500

    add interaction with Lex

--- backend/AIChat/index.js
+++ backend/AIChat/index.js
@@ -1,25 +1,64 @@
-exports.handler = function(event, context, callback) {
+'use strict';
 
-    var text = event.messages[0].unstructured.text;
-    var answer = "";
-    
-    if (text === 'Hello') {
-        answer = 'Hi there, how can I help?';
-    } else {
-        answer = 'Please say hello to me';
-    }
-    console.log(text);
+var AWS = require('aws-sdk');
+var lexruntime = new AWS.LexRuntime();
 
-    const response = {
+// Build unstructured message
+function buildUnstructuredMessage(text) {
+    let response = {
       statusCode: 200,
-      // HERE'S THE CRITICAL PART
       headers: {
         "Access-Control-Allow-Credentials": true,
         "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(answer)
+      body: JSON.stringify(text)
     };
+    return response;
+}
 
-    callback(null, response);
+
+// Send message to Lex
+const callLex = (message) => {
+    return new Promise((resolve, reject) => {
+        var params = {
+            botAlias: 'reservation',
+            /* required */
+            botName: 'Booking',
+            /* required */
+            inputText: message,
+            /* required */
+            userId: '1800',
+            /* required */
+        };
+
+        // Send a request using LexRuntime
+        lexruntime.postText(params, function(err, data) {
+            if (err) reject(err);
+            else {
+              console.log("Message sent to Lex. Succeed!!")
+              resolve(data);
+            } // successful response
+        });
+    });
+};
+
+// Handler
+exports.handler = (event, context, callback) => {
+  console.log('request on AIChat');
+
+  try {
+    let responseMessages = [];
+    const newMessage = event.messages[0].unstructured.text;
+    console.log(`get message ${newMessage}`);
+    
+    // Send the message to Lex'
+    callLex(newMessage).then((lexResponse) => {
+      responseMessages = buildUnstructuredMessage(lexResponse.message);
+      callback(null, responseMessages);
+    });
+  } catch (error) {
+    console.log("error happened in AIChat! ", error);
+    callback(error);
+  }
 };
